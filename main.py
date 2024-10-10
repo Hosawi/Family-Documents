844fa6cba92016bb79841a54a1a49cc778ebb596
Hosawi <hosawi2001@gmail.com>
2024-10-09 23:51:42 -0500

    application scenario
    User class is tested, and other are being tested. The application scenario is set as follows:
    
    I want to create a loop within main to show a menu with the following options:
    (1) Create a new document.
    (2) Search for a document.
    (3) Update document data.
    (X) to exit the system.
    
    I'll ask the user to select and enter a number from the following list numbers.

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 # This file runs by the "end user" and imports other modules to make use of their functions/classes.
 
 from user import User 
-from document import Document 
-from documentRepository import DocumentRepository 
-from metadataManager import MetadataManager
-from fileUploader import FileUploader
+#from document import Document 
+#from documentRepository import DocumentRepository 
+#from metadataManager import MetadataManager
+#from fileUploader import FileUploader
+import os # To use some OS functions 
 
 
-def main():
+#def main():
     
 #-----  My test center ------: 
-###v Runnin functions W/ simulated user input ###
+### Testing User input ### 
+def clear_screen():
+    # This works for Windows and Unix-based systems
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def welcome_user(user):
+    print(f"Welcome, {user.username}!, email: ,{user.email}, pass:,{user.password} ")
+  
+## ------ 
+def main():
+    user = User("Abdul Hosawi", "hosawi@iastate.edu", "I can't give you my secure_password :-)")
+    clear_screen()
+    welcome_user(user)
+
+""""
+I want to create a loop within main to show a menu with the following options:
+(1) Create a new document.
+(2) Search for a document.
+(3) Update document data.
+(X) to exit the system.
+User is asked to select a number from the menu.
+"""
 # ======  Testing document class methods =========
+
+#---------------------------------------
+if __name__ == "__main__":
+    main()
+
+
 # ======  Testing user class methods =========
 # ======  Testing documnet class methods =========
 # ======  Testing documentRepository class methods =========
 # ======  Testing metadataManager class methods =========
 # ======  Testing FileUploader class methods =========
- if __name__ == "__main__":
-    main()
+ #if __name__ == "__main__":
+  #  main()
 
 """
  Sequence Diagram  
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
-# info 
+# info
+# # 
 class User:
     def __init__(self, username, email, password):
         # Based on the feedback, the variable 'id'  has been deleted.
@@ -7,9 +8,5 @@ class User:
         self.password = password
 
 
-    def __str__(self):
-        return f"User( username={self.username}, email={self.email})"
-
-# Test. In the future, the test will be from file main.py.
-user1 = User("Hosawi", "Hoawi@iastate.edu", "MyPassword123")
-print(user1)
+ #   def __str__(self):
+      #  return f"User( username={self.username}, email={self.email})"
